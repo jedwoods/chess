7d58c497a679b0c775ecf8f90c1f9c11470513e4
jedwoods <jedcwoods@gmail.com>
2024-09-07 15:36:36 -0600

    stashing commits

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,6 +1,7 @@
 package chess;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -42,16 +43,53 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-
         currentBoard.replaceAll(ignored -> (new ArrayList<>(8)));
         for (int i = 0; i < 8; i++){
             if (i == 0){
                 List<ChessPiece> row = currentBoard.getFirst();
-//              for (int j = 0; j < 8; j++){
-//                  row.set();
-//
-//              }
+                row.set(0, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+                row.set(1, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+                row.set(2, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+                row.set(3, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+                row.set(4, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+                row.set(5, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+                row.set(6, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+                row.set(1, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+              }
+            else if (i == 1){
+                List<ChessPiece> row = currentBoard.get(i);
+                row.replaceAll(ignore -> new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+
+            }
+            else if (i == 6){
+                List<ChessPiece> row = currentBoard.get(i);
+                row.replaceAll(ignore -> new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            }
+            else if (i == 7){
+                List<ChessPiece> row = currentBoard.get(i);
+                row.set(0, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+                row.set(1, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+                row.set(2, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+                row.set(3, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+                row.set(4, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+                row.set(5, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+                row.set(6, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+                row.set(1, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+
             }
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that=(ChessBoard) o;
+        return Objects.equals(currentBoard, that.currentBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(currentBoard);
     }
 }
