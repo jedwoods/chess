f615fb9a14e366087b1a1c4a6eda603e26509fba
jedwoods <jedcwoods@gmail.com>
2024-10-23 19:35:49 -0600

    cleaned up function to not be so nested

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -73,31 +73,33 @@ public class DatabaseManager {
         }
     }
 
-    public static int executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                  switch (param) {
-                    case String p -> ps.setString(i + 1, p);
-                    case Integer p -> ps.setInt(i + 1, p);
-//                    case PetType p -> ps.setString(i + 1, p.toString());
-                    case null -> ps.setNull(i + 1, NULL);
-                    default -> {
-                    }
-                  }
-                }
-                ps.executeUpdate();
 
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-
-                return 0;
+public static int executeUpdate(String statement, Object... params) throws DataAccessException {
+    try (var conn = DatabaseManager.getConnection()) {
+        try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+            setParameters(ps, params);
+            ps.executeUpdate();
+            var rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
             }
-        } catch (SQLException e) {
-            throw new DataAccessException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+            return 0;
+        }
+    } catch (SQLException e) {
+        throw new DataAccessException(500, String.format("Unable to update database: %s, %s", statement, e.getMessage()));
+    }
+}
+
+    private static void setParameters(java.sql.PreparedStatement ps, Object... params) throws SQLException {
+        for (var i = 0; i < params.length; i++) {
+            var param = params[i];
+            switch (param) {
+                case String p -> ps.setString(i + 1, p);
+                case Integer p -> ps.setInt(i + 1, p);
+                case null -> ps.setNull(i + 1, java.sql.Types.NULL);
+                default -> {
+                }
+            }
         }
     }
 
