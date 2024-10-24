9a07ded05a7b7393f074fda9ea295b9bba9feaf6
jedwoods <jedcwoods@gmail.com>
2024-10-23 18:40:16 -0600

    added more tests

---
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -0,0 +1,20 @@
+package dataaccess;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataAccessTest {
+
+  @Test
+  void clear() {
+  }
+
+  @Test
+  void getUser() {
+  }
+
+  @Test
+  void isEmpty() {
+  }
+}
