1bfe4f5094ca7943e544ed38728b1a785abdef2b
jedwoods <jedcwoods@gmail.com>
2024-10-30 11:32:17 -0600

    added run function on the repl

--- client/src/main/java/Repl.java
+++ client/src/main/java/Repl.java
@@ -1,2 +1,44 @@
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+
 public class Repl {
+  ServerFacade client;
+
+  public Repl(String site){
+
+    this.client = new ServerFacade(site);
+
+  }
+
+
+  public void run() {
+    System.out.println("♘ Welcome to the Chess. Sign in to start.");
+    System.out.print(client.help());
+
+    Scanner scanner = new Scanner(System.in);
+    var result = "";
+    while (!result.equals("quit")) {
+      printPrompt();
+      String line = scanner.nextLine();
+
+      try {
+        result = client.eval(line);
+        System.out.print(SET_TEXT_COLOR_BLUE + result);
+      } catch (Throwable e) {
+        var msg = e.toString();
+        System.out.print(msg);
+      }
+    }
+    System.out.println();
+  }
+
+  private void printPrompt() {
+    System.out.print("\n" + RESET + ">>> " + SET_TEXT_COLOR_GREEN);
+  }
 }
+
+
+
+
--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,4 +1,16 @@
+import static ui.EscapeSequences.*;
+
 public class ServerFacade {
+
+
+  public ServerFacade(String site) {
+
+  }
+
+  public String help(){
+    return "";
+  }
+
 
 
 }
