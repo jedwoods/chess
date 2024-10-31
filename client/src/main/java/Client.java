b4814922d525021f7d009f3fcc21be770b7cd173
jedwoods <jedcwoods@gmail.com>
2024-10-31 10:29:16 -0600

    restored client

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,10 +1,22 @@
-import com.google.gson.Gson;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import com.google.gson.Gson;
+import dataaccess.gamedatabase.GameData;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static ui.EscapeSequences.*;
 
 public class Client {
   ServerFacade server;
   State state;
+  HashSet<GameData> games;
 
 
 
@@ -16,7 +28,7 @@ public class Client {
   public String help() {
     if (state == State.SIGNEDOUT) {
       return """
-                    - signIn <yourname>
+                    - login <username> <password>
                     - quit - get outta here
                     - help
                     - register <name> <password> <email>
@@ -24,8 +36,8 @@ public class Client {
     }
     return """
                 - list - gets all games
-                - create game <game name>
-                - play game <game ID> [white|black]
+                - create <game name>
+                - playGame <game ID> [white|black]
                 - observe game <game ID>
                 - signOut
                 - quit
@@ -41,10 +53,12 @@ public class Client {
       var params = Arrays.copyOfRange(tokens, 1, tokens.length);
       return switch (cmd) {
         case "logout" -> logout();
-        case "sign in" -> login(params);
+        case "login" -> login(params);
         case "list" -> listGames();
-        case "play game" -> joinGame(params);
-        case "observe game" -> observe();
+        case "playgame" -> joinGame(params);
+        case "create" -> newGame(params);
+//        case "observegame" -> observe();
+        case "register" -> register(params);
         case "quit" -> "quit";
         default -> help();
       };
@@ -53,25 +67,109 @@ public class Client {
     }
   }
 
+  private String newGame(String[] params) throws ResponseException {
+    assertSignedIn();
+    if (params.length == 1) {
+      ResponseObject returned = server.createGame(params[0]);
+      return returned.message();
+    }
+    else{
+      throw new ResponseException(400, "Expected: <gameName>");
+    }
+  }
+
   public String listGames() throws ResponseException {
     assertSignedIn();
-    var games = server.listGames();
+    this.games = server.listGames();
     var result = new StringBuilder();
     var gson = new Gson();
-    for (var game : games) {
-      result.append(gson.toJson(game)).append('\n');
+    int i = 1;
+    if (games.isEmpty()){
+      System.out.println("No games available");
     }
+    for (var game : this.games) {
+      result.append(String.format("%d. %s%n", i, game.gameName())).append("\n");
+      i++;
+    }
+
     return result.toString();
   }
 
   public String joinGame(String[] params) throws ResponseException {
     assertSignedIn();
     if (params.length == 2) {
-      ResponseObject response = server.joinGame(params[0], params[0]);
-      return "";
+//      ResponseObject response = server.joinGame(params[0], params[0]);
+      int id =  Integer.parseInt(params[0]);
+      int i = 1;
+      for (var game : this.games){
+        if (i == id){
+          ResponseObject response = server.joinGame(params[1], game.gameID());
+          return printBoard(game.game());
+        }
+      }
+      throw new ResponseException(400, "invalid ID");
     } else{
       throw new ResponseException(400, "Expected: <your name> <password>");
     }
+  }
+
+
+  public String getPiece(ChessPiece piece){
+    if (piece == null){
+      return " ";
+    } else if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
+      return switch (piece.getPieceType()) {
+        case ChessPiece.PieceType.PAWN -> BLACK_PAWN;
+        case ChessPiece.PieceType.ROOK -> BLACK_ROOK;
+        case ChessPiece.PieceType.KNIGHT -> BLACK_KNIGHT;
+        case ChessPiece.PieceType.KING -> BLACK_KING;
+        case ChessPiece.PieceType.QUEEN -> BLACK_QUEEN;
+        case ChessPiece.PieceType.BISHOP -> BLACK_BISHOP;
+      };
+    }else{
+      return switch (piece.getPieceType()) {
+        case ChessPiece.PieceType.PAWN -> WHITE_PAWN;
+        case ChessPiece.PieceType.ROOK -> WHITE_ROOK;
+        case ChessPiece.PieceType.KNIGHT -> WHITE_KNIGHT;
+        case ChessPiece.PieceType.KING -> WHITE_KING;
+        case ChessPiece.PieceType.QUEEN -> WHITE_QUEEN;
+        case ChessPiece.PieceType.BISHOP -> WHITE_BISHOP;
+      };
+    }
+  }
+
+
+  public String printBoard(ChessGame game){
+    List<String> letters = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h"));
+    ChessBoard board = game.getBoard();
+    var result = new StringBuilder();
+    result.append(" ");
+    for (String c : letters){
+      result.append(c);
+    }
+    result.append(" \n");
+    String color = SET_BG_COLOR_LIGHT_GREY;
+    String secondary = SET_BG_COLOR_DARK_GREEN;
+    for (int j = 1; j<9; j++ ){
+      result.append(RESET_TEXT_COLOR).append(j+1);
+      for (int k = 1; k<9; k++ ){
+        ChessPiece piece = board.getPiece(new ChessPosition(j,k));
+        if (j %2 == k %2){
+          result.append(color).append(getPiece(piece));
+        }else{
+          result.append(secondary).append(getPiece(piece));
+        }
+      }
+      result.append(RESET).append(j+1);
+      result.append("\n");
+    }
+    result.append(" ");
+    for (String c : letters){
+      result.append(c);
+    }
+
+    return result.toString();
+
   }
 
 
@@ -87,9 +185,19 @@ public class Client {
   }
 
 
+
+  public String register(String[] params) throws ResponseException {
+    if (params.length == 3) {
+      ResponseObject response= server.register(params[0], params[1], params[2]);
+      state=State.SIGNEDIN;
+      return "";
+    } else {
+      throw new ResponseException(400, "Expected: <your name> <password>");
+    }
+  }
   public String login(String[] params) throws ResponseException {
     if (params.length == 2) {
-      ResponseObject response = server.login(params[0], params[0]);
+      ResponseObject response = server.login(params[0], params[1]);
       state = State.SIGNEDIN;
       return "";
     } else{
@@ -103,7 +211,5 @@ public class Client {
       throw new ResponseException(400, "You must sign in");
     }
   }
-
-
 
 }
