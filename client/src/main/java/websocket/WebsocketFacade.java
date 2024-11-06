b8a5d779a3e8e25281088663f009a61ac30a2515
jedwoods <jedcwoods@gmail.com>
2024-11-06 14:31:10 -0700

    added basic functions

--- client/src/main/java/websocket/WebsocketFacade.java
+++ client/src/main/java/websocket/WebsocketFacade.java
@@ -1,10 +1,26 @@
 package websocket;
 
-public class WebsocketFacade extends EndPoint{
+
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.glassfish.tyrus.core.wsadl.model.Endpoint;
+
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+//@WebSocket
+public class WebsocketFacade extends Endpoint {
 
 
 
-  @Override
+  public void send(String msg) throws Exception {}
+
   public void onOpen(Session session, EndpointConfig endpointConfig) {
   }
+
+
 }
