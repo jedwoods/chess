0ccec5a95070a9a064138e42081159e68cc92274
jedwoods <jedcwoods@gmail.com>
2024-11-06 14:19:20 -0700

    added dependencies

--- client/src/main/java/websocket/WebsocketFacade.java
+++ client/src/main/java/websocket/WebsocketFacade.java
@@ -1,4 +1,10 @@
 package websocket;
 
-public class WebsocketFacade {
+public class WebsocketFacade extends EndPoint{
+
+
+
+  @Override
+  public void onOpen(Session session, EndpointConfig endpointConfig) {
+  }
 }
