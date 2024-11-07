7f4efe878eaab938ccf2db6629b83bae0281862e
jedwoods <jedcwoods@gmail.com>
2024-11-07 06:46:08 -0700

    added makemove command

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -3,6 +3,8 @@ import server.websocket.Connection;
 import websocket.messages.Notification;
 
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,7 +21,7 @@ public class ConnectionManager {
     connections.remove(authToken);
   }
 
-  public void broadcast(String excludeToken, Notification notification) throws IOException {
+  public void broadcast(String excludeToken, ServerMessage notification) throws IOException {
     var removeList = new ArrayList<Connection>();
     for (var c : connections.values()) {
       if (c.session.isOpen()) {
@@ -31,6 +33,23 @@ public class ConnectionManager {
       }
     }
 
+    // Clean up any connections that were left open.
+    for (var c : removeList) {
+      connections.remove(c.authToken);
+    }
+  }
+
+  public void sendMessage(String token, ServerMessage notification) throws IOException {
+    var removeList = new ArrayList<Connection>();
+    for (var c : connections.values()) {
+      if (c.session.isOpen()) {
+        if (c.authToken.equals(token)) {
+          c.send(notification.toString());
+        }
+      } else {
+        removeList.add(c);
+      }
+    }
     // Clean up any connections that were left open.
     for (var c : removeList) {
       connections.remove(c.authToken);
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,7 @@
 package server.websocket;
 
+import chess.ChessGame;
+import chess.InvalidMoveException;
 import com.google.gson.Gson;
 import dataaccess.authdatabase.AuthToken;
 import dataaccess.gamedatabase.GameData;
@@ -7,7 +9,10 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import server.Service;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
+import websocket.messages.Error;
+import websocket.messages.LoadGameMessage;
 import websocket.messages.Notification;
 import websocket.messages.ServerMessage;
 
@@ -68,7 +73,26 @@ public class WebSocketHandler {
       case RESIGN -> resign(command.getAuthToken(), session);
       case CONNECT -> connect(command.getAuthToken(), session);
       case LEAVE -> leave(command.getAuthToken(), session);
+      case MAKE_MOVE -> makeMove(message);
     }
+  }
+
+  private void makeMove(String message) throws IOException {
+    MakeMoveCommand command=  new Gson().fromJson(message, MakeMoveCommand.class);
+    ChessGame game = service.getDB().getGame(command.getGameID()).game();
+    try {
+      game.makeMove(command.getMove());
+    } catch (InvalidMoveException e) {
+      String errorM = String.format("%s is invalid", command.getMove().toString());
+      connections.sendMessage(command.getAuthToken(), new Error(ServerMessage.ServerMessageType.ERROR, errorM));
+      return;
+    }
+    String name = service.getDB().getSession(command.getAuthToken()).username();
+    String note = String.format("%s has left your game", name);
+    var notification = new Notification(ServerMessage.ServerMessageType.NOTIFICATION, note);
+    connections.broadcast(command.getAuthToken(), notification);
+    var move = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, game);
+    connections.broadcast(command.getAuthToken(), move);
   }
 
 }
