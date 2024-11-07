b62a10651b06c05b06ee1e86784a1798d86cb0c0
jedwoods <jedcwoods@gmail.com>
2024-11-07 14:17:28 -0700

    added assertcord

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,21 +1,13 @@
 package ui;
-import websocket.*;
-import chess.ChessBoard;
-import chess.ChessGame;
-import chess.ChessPiece;
-import chess.ChessPosition;
+import chess.*;
 import com.google.gson.Gson;
-import dataaccess.userdatabase.User;
 import facade.ServerFacade;
 import records.GameData;
 import records.ResponseException;
 import records.ResponseObject;
 import websocket.WebsocketFacade;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 import static ui.EscapeSequences.*;
 
@@ -33,52 +25,51 @@ public class Client {
   ChessGame.TeamColor color;
 
 
-
   public Client(String site, ServerObserver observer) {
-    this.server = new ServerFacade(site);
-    this.state = State.SIGNEDOUT;
-    this.letters = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
-    this.playing = GameState.NOTPLAYING;
-    this.observer = observer;
-    this.site = site;
+    this.server=new ServerFacade(site);
+    this.state=State.SIGNEDOUT;
+    this.letters=Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
+    this.playing=GameState.NOTPLAYING;
+    this.observer=observer;
+    this.site=site;
 
   }
 
 
   public String help() {
-    if (this.playing == GameState.PLAYING){
+    if (this.playing == GameState.PLAYING) {
       return """
-                    - Help
-                    - Redraw
-                    - Leave
-                    - Move
-                    - Resign
-                    - HighlightMoves
-                    """;
+              - Help
+              - Redraw
+              - Leave
+              - Move
+              - Resign
+              - HighlightMoves
+              """;
     }
     if (state == State.SIGNEDOUT) {
       return """
-                    - login <username> <password>
-                    - quit - get outta here
-                    - help
-                    - register <name> <password> <email>
-                    """;
+              - login <username> <password>
+              - quit - get outta here
+              - help
+              - register <name> <password> <email>
+              """;
     }
     return """
-                - list - gets all games
-                - create <game name>
-                - playGame <game ID> [white|black]
-                - observe <game ID>
-                - logout
-                - quit
-                """;
+            - list - gets all games
+            - create <game name>
+            - playGame <game ID> [white|black]
+            - observe <game ID>
+            - logout
+            - quit
+            """;
   }
 
   public String eval(String input) {
     try {
-      var tokens = input.toLowerCase().split(" ");
-      var cmd = (tokens.length > 0) ? tokens[0] : "help";
-      var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+      var tokens=input.toLowerCase().split(" ");
+      var cmd=(tokens.length > 0) ? tokens[0] : "help";
+      var params=Arrays.copyOfRange(tokens, 1, tokens.length);
       return switch (cmd) {
         case "logout" -> logout();
         case "login" -> login(params);
@@ -91,7 +82,7 @@ public class Client {
         case "leave" -> leave();
 //        case "move" -> makeMove(params);
 //        case "resign" -> resign(params);
-//        case "highlightmoves" -> listMoves(params);
+        case "highlightmoves" -> listMoves(params);
         case "quit" -> "quit";
         default -> help();
       };
@@ -100,16 +91,17 @@ public class Client {
     }
   }
 
+
   private String redraw() throws ResponseException {
     assertSignedIn();
-    if (playing != GameState.PLAYING){
+    if (playing != GameState.PLAYING) {
       return "you are not playing a game";
     }
 
     this.server.listGames();
-    for (var c:this.games){
-      if (c.gameID() == this.gameID){
-        if (this.color == ChessGame.TeamColor.WHITE){
+    for (var c : this.games) {
+      if (c.gameID() == this.gameID) {
+        if (this.color == ChessGame.TeamColor.BLACK) {
           return printBlackBoard(c.game());
         }
         return printWhite(c.game());
@@ -121,36 +113,35 @@ public class Client {
   private String newGame(String[] params) throws ResponseException {
     assertSignedIn();
     if (params.length == 1) {
-      ResponseObject returned = server.createGame(params[0]);
+      ResponseObject returned=server.createGame(params[0]);
       return returned.message();
-    }
-    else{
+    } else {
       throw new ResponseException(400, "Expected: <gameName>");
     }
   }
 
   public String listGames() throws ResponseException {
     assertSignedIn();
-    this.games = server.listGames();
-    var result = new StringBuilder();
-    var gson = new Gson();
-    int i = 1;
-    if (games.isEmpty()){
+    this.games=server.listGames();
+    var result=new StringBuilder();
+    var gson=new Gson();
+    int i=1;
+    if (games.isEmpty()) {
       System.out.println("No games available");
     }
     for (var game : this.games) {
       result.append(String.format("%d. %s%n", i, game.gameName()));
 
       result.append("WHITE PLAYER: ");
-      if (Objects.isNull(game.whiteUsername())){
+      if (Objects.isNull(game.whiteUsername())) {
         result.append("None");
-      }else{
+      } else {
         result.append(game.whiteUsername());
       }
       result.append(" BLACK PLAYER: ");
-      if (Objects.isNull(game.blackUsername())){
+      if (Objects.isNull(game.blackUsername())) {
         result.append("None");
-      }else{
+      } else {
         result.append(game.blackUsername());
       }
       result.append("\n");
@@ -170,47 +161,48 @@ public class Client {
       } catch (NumberFormatException e) {
         throw new ResponseException(401, "input must be a valid index");
       }
-      int i = 1;
-      for (var game : this.games){
-        if (i == id){
-          if (params[1].strip().equalsIgnoreCase("WHITE")){
-            this.color =ChessGame.TeamColor.WHITE;
-            if (username.equals(game.whiteUsername())){
-              this.playing = GameState.PLAYING;
-              this.gameID = game.gameID();
-              ws = new WebsocketFacade(this.site, observer);
-              ws.playGame(params[1], this.server.getAuthToken(),game.gameID());
+      int i=1;
+      for (var game : this.games) {
+        if (i == id) {
+          if (params[1].strip().equalsIgnoreCase("WHITE")) {
+            this.color=ChessGame.TeamColor.WHITE;
+            if (username.equals(game.whiteUsername())) {
+              this.playing=GameState.PLAYING;
+              this.gameID=game.gameID();
+              ws=new WebsocketFacade(this.site, observer);
+              ws.playGame(params[1], this.server.getAuthToken(), game.gameID());
               return printWhite(game.game());
             }
           }
-          if (params[1].toUpperCase().strip().equals("BLACK")){
-            this.color =ChessGame.TeamColor.BLACK;
-            if (username.equals(game.blackUsername())){
-              this.playing = GameState.PLAYING;
-              ws = new WebsocketFacade(this.site, observer);
-              ws.playGame(params[1], this.server.getAuthToken(),game.gameID());
-              this.gameID = game.gameID();
+          if (params[1].toUpperCase().strip().equals("BLACK")) {
+            this.color=ChessGame.TeamColor.BLACK;
+            if (username.equals(game.blackUsername())) {
+              this.playing=GameState.PLAYING;
+              ws=new WebsocketFacade(this.site, observer);
+              ws.playGame(params[1], this.server.getAuthToken(), game.gameID());
+              this.gameID=game.gameID();
               return printBlackBoard(game.game());
             }
           }
-          ResponseObject response = server.joinGame(params[1].toUpperCase(), game.gameID());
-          this.playing = GameState.PLAYING;
-          ws = new WebsocketFacade(this.site, observer);
-          ws.playGame(params[1], this.server.getAuthToken(),game.gameID());
-          this.gameID = game.gameID();
-          if (this.color == ChessGame.TeamColor.WHITE){
-          return printWhite(game.game());}
-          else{
+          ResponseObject response=server.joinGame(params[1].toUpperCase(), game.gameID());
+          this.playing=GameState.PLAYING;
+          ws=new WebsocketFacade(this.site, observer);
+          ws.playGame(params[1], this.server.getAuthToken(), game.gameID());
+          this.gameID=game.gameID();
+          if (this.color == ChessGame.TeamColor.WHITE) {
+            return printWhite(game.game());
+          } else {
             return printBlackBoard(game.game());
           }
         }
         i++;
       }
       throw new ResponseException(400, "invalid ID");
-    } else{
+    } else {
       throw new ResponseException(400, "Expected: playgame <gameID> <[BLACK|WHITE]>");
     }
   }
+
   public String observe(String[] params) throws ResponseException {
     assertSignedIn();
     int id;
@@ -221,27 +213,27 @@ public class Client {
       } catch (NumberFormatException e) {
         throw new ResponseException(401, "input must be a valid index");
       }
-      int i = 1;
-      for (var game : this.games){
-        if (i == id){
+      int i=1;
+      for (var game : this.games) {
+        if (i == id) {
 //          records.ResponseObject response = server.joinGame(null, game.gameID());
           return printBoard(game.game());
         }
         i++;
       }
       throw new ResponseException(400, "invalid ID");
-    } else{
+    } else {
       throw new ResponseException(400, "Expected: <your name> <password>");
     }
   }
 
 
-  public String getPiece(ChessPiece piece){
-    if (piece == null){
+  public String getPiece(ChessPiece piece) {
+    if (piece == null) {
       return "   ";
     } else if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
       return getString(piece, BLACK_PAWN, BLACK_ROOK, BLACK_KNIGHT, BLACK_KING, BLACK_QUEEN, BLACK_BISHOP);
-    }else{
+    } else {
       return getString(piece, WHITE_PAWN, WHITE_ROOK, WHITE_KNIGHT, WHITE_KING, WHITE_QUEEN, WHITE_BISHOP);
     }
   }
@@ -258,13 +250,13 @@ public class Client {
   }
 
 
-  public String printBlackBoard(ChessGame game){
-    ChessBoard board = game.getBoard();
-    var result = new StringBuilder();
+  public String printWhite(ChessGame game) {
+    ChessBoard board=game.getBoard();
+    var result=new StringBuilder();
 
     appendColumnLabels(result);
 
-    for (int row = 8; row >= 1; row--) {
+    for (int row=8; row >= 1; row--) {
       reverseRow(result, row, board);
     }
 
@@ -273,12 +265,12 @@ public class Client {
     return result.toString();
   }
 
-  public String printWhite(ChessGame game){
-    ChessBoard board = game.getBoard();
-    var result = new StringBuilder();
+  public String printBlackBoard(ChessGame game) {
+    ChessBoard board=game.getBoard();
+    var result=new StringBuilder();
     appendReverseLabels(result);
 
-    for (int row = 1; row <= 8; row++) {
+    for (int row=1; row <= 8; row++) {
       appendRow(result, row, board);
     }
     appendReverseLabels(result);
@@ -287,12 +279,12 @@ public class Client {
   }
 
   public String printBoard(ChessGame game) {
-    ChessBoard board = game.getBoard();
-    var result = new StringBuilder();
+    ChessBoard board=game.getBoard();
+    var result=new StringBuilder();
 
     appendColumnLabels(result);
 
-    for (int row = 8; row >= 1; row--) {
+    for (int row=8; row >= 1; row--) {
       reverseRow(result, row, board);
     }
 
@@ -303,7 +295,7 @@ public class Client {
 
     appendReverseLabels(result);
 
-    for (int row = 1; row <= 8; row++) {
+    for (int row=1; row <= 8; row++) {
       appendRow(result, row, board);
     }
     appendReverseLabels(result);
@@ -340,37 +332,36 @@ public class Client {
   }
 
 
-
-  public String logout() throws  ResponseException{
+  public String logout() throws ResponseException {
     assertSignedIn();
     try {
       server.logout();
-      state = State.SIGNEDOUT;
+      state=State.SIGNEDOUT;
       return "You have been logged out";
-    } catch (Exception e){
+    } catch (Exception e) {
       throw new ResponseException(500, "Failed to logout");
     }
   }
 
 
-
   public String register(String[] params) throws ResponseException {
     if (params.length == 3) {
-      ResponseObject response= server.register(params[0], params[1], params[2]);
-      username = params[0];
+      ResponseObject response=server.register(params[0], params[1], params[2]);
+      username=params[0];
       state=State.SIGNEDIN;
       return "";
     } else {
       throw new ResponseException(400, "Expected: <your name> <password> <email>");
     }
   }
+
   public String login(String[] params) throws ResponseException {
     if (params.length == 2) {
-      ResponseObject response = server.login(params[0], params[1]);
-      state = State.SIGNEDIN;
-      username = params[0];
+      ResponseObject response=server.login(params[0], params[1]);
+      state=State.SIGNEDIN;
+      username=params[0];
       return "";
-    } else{
+    } else {
       throw new ResponseException(400, "Expected: <your name> <password> <email>");
     }
 
@@ -383,30 +374,29 @@ public class Client {
   }
 
 
-
-  public void boardBuilder(StringBuilder result, int row, ChessBoard board){
+  public void boardBuilder(StringBuilder result, int row, ChessBoard board) {
 //      result.append(" ").append(row).append(" ");
 
-      for (int col = 8; col >= 1; col--) {
-        ChessPiece piece = board.getPiece(new ChessPosition(row, col));
+    for (int col=8; col >= 1; col--) {
+      ChessPiece piece=board.getPiece(new ChessPosition(row, col));
 
-        if ((row + col) % 2 == 0) {
-          result.append(SET_BG_COLOR_LIGHT_GREY);
-        } else {
-          result.append(SET_BG_COLOR_DARK_GREEN);
-        }
-
-        result.append(" ").append(getPiece(piece)).append(" ");
+      if ((row + col) % 2 == 0) {
+        result.append(SET_BG_COLOR_LIGHT_GREY);
+      } else {
+        result.append(SET_BG_COLOR_DARK_GREEN);
       }
-      result.append(RESET_BG_COLOR).append(" ");
+
+      result.append(" ").append(getPiece(piece)).append(" ");
+    }
+    result.append(RESET_BG_COLOR).append(" ");
   }
 
 
-  public void reversedBuilder(StringBuilder result, int row, ChessBoard board){
+  public void reversedBuilder(StringBuilder result, int row, ChessBoard board) {
 //      result.append(" ").append(row).append(" ");
 
-    for (int col = 1; col <= 8; col++) {
-      ChessPiece piece = board.getPiece(new ChessPosition(row, col));
+    for (int col=1; col <= 8; col++) {
+      ChessPiece piece=board.getPiece(new ChessPosition(row, col));
 
       if ((row + col) % 2 == 0) {
 
@@ -420,15 +410,61 @@ public class Client {
     result.append(RESET_BG_COLOR).append(" ");
   }
 
-public String leave() throws ResponseException {
-  assertSignedIn();
-  if (playing != GameState.PLAYING){
-    return "you are not playing a game";
+  public String leave() throws ResponseException {
+    assertSignedIn();
+    if (playing != GameState.PLAYING) {
+      return "you are not playing a game";
+    }
+    ws.leaveGame(server.getAuthToken(), this.gameID);
+    gameID=-1;
+    ws=null;
+    playing=GameState.NOTPLAYING;
+    return "You have left the game";
   }
-  ws.leaveGame(server.getAuthToken(), this.gameID);
-  gameID = -1;
-  ws = null;
-  playing = GameState.NOTPLAYING;
-  return "You have left the game";
-}
+
+  private String listMoves(String[] params) throws ResponseException {
+    if (params.length != 1) {
+      throw new ResponseException(500, "expected: HighlightMoves <location>");
+    }
+    ChessPosition start = assertCord(params[0]);
+    for (var c : this.games) {
+      if (c.gameID() == this.gameID) {
+        if (this.color == ChessGame.TeamColor.BLACK) {
+          Collection<ChessMove> moves = c.game().validMoves(start);
+          return printValidBoard(start,moves);
+        }
+      }
+    }
+
+  }
+
+  private String printValidBoard(ChessPosition start, Collection<ChessMove> moves) {
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public ChessPosition assertCord(String move) throws ResponseException {
+    if (move.length() != 2){
+      throw new ResponseException(500, "expect: <row><col>");
+    }
+    char col = move.charAt(0);
+    int row = move.charAt(1) - 'a' + 1;
+
+    if(col >= 'a' && col <= 'h' && row >= 1 && row <= 8){
+      return new ChessPosition(row - 'a' + 1, col);
+    }
+    throw new ResponseException(500, "position out of board");
+  }
 }
